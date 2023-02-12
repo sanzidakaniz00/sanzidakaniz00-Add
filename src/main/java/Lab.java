239d58ce0e9e982280d705dbf637b49650bc2555
Sanzida Kaniz <snzdknz@gmail.com>
2023-02-12 16:10:30 +0000

    Coding Challenge:Add

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -10,7 +10,10 @@ public class Lab {
      * @param b second number to be added.
      * @return the sum of a and b.
      */
+
+    
     public int addNumbers(int a, int b){
-        return 0;
+        return a+b;
     }
+   
 }
